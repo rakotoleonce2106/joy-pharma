f6abc1e04796b950beb03c14b251ac57eb25934f
rakotoleonce <jeanleonce@newmanstech.com>
2025-04-13 19:34:07 +0300

    initialize projet and template

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\Bootstrap\DefaultLayoutController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DashboardController extends DefaultLayoutController
+{
+    #[Route('/', name:'admin_dashboard')]
+    public function index(): Response
+    {
+        # Include vendors and javascript files for dashboard widgets
+        $this->theme->addVendors(['amcharts', 'amcharts-maps', 'amcharts-stock']);
+
+        return $this->render('pages/dashboards/index.html.twig');
+    }
+}
